8c3206faa49a2546f81b27c3373af1778622a9d7
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2024-02-23 12:58:54 +0100

    UnderWaterTrash Tutorial scene

--- IntroScene.js
+++ IntroScene.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-import Flag from './assets/flag.png';
+import Flag from './assets/Bubble1.png';
 import Hovedpraemie from './assets/tekst_hovedpraemie.png';
 import Spilogvind from './assets/knap_spilogvind.png';
 
@@ -21,60 +21,64 @@ export default class IntroScene extends Phaser.Scene {
     create() {
 
 
-        const screenCenterX = this.cameras.main.width / 2;
-        const screenCenterY = this.cameras.main.height / 2;
+        const screenCenterX = this.game.config.width / 2;
+        const screenCenterY = this.game.config.height / 2;
 
             // Pop balloner
-            const pop_tekst1 = this.add.text(screenCenterX, 100, 'POP BALLONER', {
-            fontFamily: 'DIN',
-            color: 'white',
-            font: 'bold 88px DIN',
-            backgroundColor: '#164196',
-            }).setPadding({x:10}).setDepth(1).setOrigin(0.5, 0.5);
+            // const pop_tekst1 = this.add.text(screenCenterX, 100, 'POP BALLONER', {
+            // fontFamily: 'DIN',
+            // color: 'white',
+            // font: 'bold 88px DIN',
+            // backgroundColor: '#164196',
+            // }).setPadding({x:10}).setDepth(1).setOrigin(0.5, 0.5);
 
             // & vind!
-            const pop_tekst2 = this.add.text(screenCenterX - pop_tekst1.width / 2, 210, '& VIND!', {
-                fontFamily: 'DIN',
-                color: 'white',
-                font: 'bold 88px DIN',
-                backgroundColor: '#e30613',
-            }).setPadding({x:10}).setDepth(1).setOrigin(0, 0.5);
+            // const pop_tekst2 = this.add.text(screenCenterX - pop_tekst1.width / 2, 210, '& VIND!', {
+            //     fontFamily: 'DIN',
+            //     color: 'white',
+            //     font: 'bold 88px DIN',
+            //     backgroundColor: '#e30613',
+            // }).setPadding({x:10}).setDepth(1).setOrigin(0, 0.5);
 
             // Hovedpræmie billede
-            const praemietekst = this.add.image(screenCenterX, 355, 'hovedpraemie');
-            praemietekst.setDepth(1).scale = 0.7;
+            // const praemietekst = this.add.image(screenCenterX, 355, 'hovedpraemie');
+            // praemietekst.setDepth(1).scale = 0.7;
 
             // Uge-præmie tekst
-            const uge_praemie_tekst1 = this.add.text(screenCenterX - pop_tekst1.width / 2, 510, 'PRÆMIER HVER UGE:', {
+            const uge_praemie_tekst1 = this.add.text(screenCenterX, 510, 'The is how you play:', {
                 fontFamily: 'DIN',
-                color: 'black',
+                color: '#97FEED',
                 font: 'bold 40px DIN',
-                }).setDepth(1).setOrigin(0, 0.5);
-            const uge_praemie_tekst2 = this.add.text(screenCenterX - pop_tekst1.width / 2, 560, '1. plads - voucher på 500 kr. til intersport.dk', {
+                }).setDepth(1).setOrigin(0.5, 0.5);
+            const uge_praemie_tekst2 = this.add.text(screenCenterX, 560, 'Click on the falling trash, to remove it from the ocean.', {
                 fontFamily: 'DIN',
-                color: 'black',
+                color: '#97FEED',
                 font: 'normal 30px DIN',
-                }).setDepth(1).setOrigin(0, 0.5);
-            const uge_praemie_tekst3 = this.add.text(screenCenterX - pop_tekst1.width / 2, 610, '2. plads - voucher på 200 kr. til intersport.dk', {
-                fontFamily: 'DIN',
-                color: 'black',
-                font: 'normal 30px DIN',
-                }).setDepth(1).setOrigin(0, 0.5);
-            const uge_praemie_tekst4 = this.add.text(screenCenterX - pop_tekst1.width / 2, 660, '3. plads - voucher på 100 kr. til intersport.dk', {
-                fontFamily: 'DIN',
-                color: 'black',
-                font: 'normal 30px DIN',
-                }).setDepth(1).setOrigin(0, 0.5);
-            const uge_praemie_tekst5 = this.add.text(screenCenterX - pop_tekst1.width / 2, 710, 'SPRÆNG FLEST BALLONER OG VÆR MED I KONKURRENCEN OM FEDE PRÆMIER!', {
-                fontFamily: 'DIN',
-                color: 'black',
-                font: 'bold 17px DIN',
-                }).setDepth(1).setOrigin(0, 0.5);
+                }).setDepth(1).setOrigin(0.5, 0.5);
+            // const uge_praemie_tekst3 = this.add.text(screenCenterX/ 2, 610, '2. plads - voucher på 200 kr. til intersport.dk', {
+            //     fontFamily: 'DIN',
+            //     color: 'black',
+            //     font: 'normal 30px DIN',
+            //     }).setDepth(1).setOrigin(0, 0.5);
+            // const uge_praemie_tekst4 = this.add.text(screenCenterX/ 2, 660, '3. plads - voucher på 100 kr. til intersport.dk', {
+            //     fontFamily: 'DIN',
+            //     color: 'black',
+            //     font: 'normal 30px DIN',
+            //     }).setDepth(1).setOrigin(0, 0.5);
+            // const uge_praemie_tekst5 = this.add.text(screenCenterX / 2, 710, 'SPRÆNG FLEST BALLONER OG VÆR MED I KONKURRENCEN OM FEDE PRÆMIER!', {
+            //     fontFamily: 'DIN',
+            //     color: 'black',
+            //     font: 'bold 17px DIN',
+            //     }).setDepth(1).setOrigin(0, 0.5);
 
 
             // Start knap
-            const startButton = this.add.image(screenCenterX, 835, 'spilogvind');
-            startButton.setDepth(1).setOrigin(0.5, 0.5).scale = 0.5;
+            const startButton = this.add.text(screenCenterX, 835, 'Start game', {
+                fontFamily: 'DIN',
+                color: '#97FEED',
+                font: 'bold 40px DIN',
+            });
+            startButton.setDepth(1).setOrigin(0.5, 0.5);
     
             startButton.setInteractive();
     
@@ -83,11 +87,11 @@ export default class IntroScene extends Phaser.Scene {
             });
 
             // Konkurrencebetingelser
-            const konkur_beting = this.add.text(screenCenterX, 925, '* Se konkurrencebetingelser her', {
-                fontFamily: 'DIN',
-                color: 'gray',
-                font: 'bold 17px DIN',
-                }).setDepth(1).setOrigin(0.5, 0.5);
+            // const konkur_beting = this.add.text(screenCenterX, 925, '* Se konkurrencebetingelser her', {
+            //     fontFamily: 'DIN',
+            //     color: 'gray',
+            //     font: 'bold 17px DIN',
+            //     }).setDepth(1).setOrigin(0.5, 0.5);
 
     }
     update(time, delta) {
@@ -103,36 +107,41 @@ export default class IntroScene extends Phaser.Scene {
     spawnFlagObject() {
         const flagObject = this.add.image(
             Phaser.Math.Between(0, this.sys.game.config.width),
-            -this.sys.game.config.height + 400, // spawner lige over browser vinduet
+            +this.sys.game.config.height + 120, // spawner lige over browser vinduet
             'flag'
-        ).setDepth(0).setScale(0.2);
+        ).setDepth(0).setScale(this.FloatBetween(0.05, 0.2));
 
         this.physics.add.existing(flagObject);
         flagObject.body.setVelocity(0, 100);
 
         
     // Set initial rotation and angular velocity
-    flagObject.rotation = Phaser.Math.DegToRad(Phaser.Math.Between(0, 360)); // Random initial rotation
-    const angularVelocity = Phaser.Math.FloatBetween(-90, 90); // Random angular velocity
+    //flagObject.rotation = Phaser.Math.DegToRad(Phaser.Math.Between(0, 360)); // Random initial rotation
+    //const angularVelocity = Phaser.Math.FloatBetween(-180, 180); // Random angular velocity
 
     // Set initial horizontal velocity
-    const horizontalVelocity = Phaser.Math.FloatBetween(-30, 30); // Random horizontal velocity
+    //const horizontalVelocity = Phaser.Math.FloatBetween(-50, 50); // Random horizontal velocity
 
     this.physics.add.existing(flagObject);
 
     // Set angular velocity
-    flagObject.body.angularVelocity = angularVelocity;
+    //flagObject.body.angularVelocity = angularVelocity;
 
     // Set horizontal velocity
-    flagObject.body.setVelocityX(horizontalVelocity);
-    flagObject.body.setVelocityY(100); // Vertical velocity
+    //flagObject.body.setVelocityX(horizontalVelocity);
+    flagObject.body.setVelocityY(Phaser.Math.Between(-50, -100)); // Vertical velocity
 
     // Optional: Add damping to gradually slow down rotation and movement
-    flagObject.body.angularDamping = 0.99;
-    flagObject.body.linearDamping = 0.99;
+    flagObject.body.angularDamping = 0.95;
+    flagObject.body.linearDamping = 0.95;
 
     // Set opacity (e.g., 0.5 for half-transparent)
-    flagObject.setAlpha(0.3);
+    flagObject.setAlpha(0.4);
     }
 
+    // Generate random float numbers
+    FloatBetween = function (min, max)
+    {
+        return Math.random() * (max - min) + min;
+    };
 }
